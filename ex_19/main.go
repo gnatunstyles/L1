813a4e2f2a6b9625d6d30a25ee4c7f0e8b4ce07a
gnatunstyles <prokofyev110@gmail.com>
2022-08-09 17:46:24 +0300

    some comments

--- ex_19/main.go
+++ ex_19/main.go
@@ -9,12 +9,15 @@ import (
 )
 
 func main() {
+	//чтение строки из буфера
 	r := bufio.NewReader(os.Stdin)
 	line, err := r.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
+	//формирование массива из заготовленной строки
 	arr := strings.Split(line[:len(line)-1], "")
+	//формирование новой строки посредством чтения массива с другой стороны
 	reversed := ""
 	for i := len(arr) - 1; i >= 0; i-- {
 		reversed += arr[i]
--- ex_20/main.go
+++ ex_20/main.go
@@ -9,11 +9,13 @@ import (
 )
 
 func main() {
+	//чтение строки из буфера
 	r := bufio.NewReader(os.Stdin)
 	line, err := r.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
+	//формирование массива
 	arr := strings.Split(line[:len(line)-1], " ")
 
 	for i := len(arr) - 1; i >= 0; i-- {
